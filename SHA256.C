ceeaf2006e0e9b390553afa9d79ed8022dc2e272
james kennedy <jameskennedy@jamess-MacBook-Air.local>
2019-03-14 18:43:54 +0000

    started sha256.C

---
+++ SHA256.C
@@ -0,0 +1,23 @@
+// James Kennedy G00341457
+
+
+
+
+#include <stdio.h>
+
+
+#include<stdint.h>
+
+void sha256();
+
+
+
+
+int main(int argc, char *argv[]){
+    return 0;
+}
+
+
+void sha256(){
+    
+}
