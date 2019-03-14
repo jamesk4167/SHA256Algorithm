8bb786d8db2e34f71580870a93df16c3b7e1419d
james kennedy <jameskennedy@jamess-MacBook-Air.local>
2019-03-14 19:42:17 +0000

    finished part one of assignment

--- SHA256.C
+++ SHA256.C
@@ -13,6 +13,9 @@ void sha256();
     uint32_t sigma_0(uint32_t x);
     uint32_t sigma_19(uint32_t x);
 
+    uint32_t rotr(uint32_t n, uint32_t x);
+    uint32_t shr(uint32_t n, uint32_t x);
+
 
 
 
@@ -60,13 +63,56 @@ void sha256(){
         sigma_1(w[t - 2]) + W[t - 7] + sigma_0(W[t - 15]) + W[t - 16]);
     }
 
+
+    a = H[0];
+    b = H[1];
+    c = H[2];
+    d = H[3];
+    e = H[4];
+    f = H[5];
+    g = H[6];
+    h = H[7];
+
+    for(t = 0; t < 64; t++){
+        T1 = h + sigma_1(e) + ch(e,f,g) + K[t] + W[t];
+        T2 = sigma_0 + Maj(a,b,c);
+        h = g;
+        g = f;
+        f = e;
+        e = d + T1;
+        d = c;
+        c = b;
+        b = a;
+        a = T1 + T2;
+
+    }
+    H[0] = a + H[0];
+    H[1] = b + H[1];
+    H[2] = b + H[2];
+    H[3] = b + H[3];
+    H[4] = b + H[4];
+    H[5] = b + H[5];
+    H[6] = b + H[6];
+    H[7] = b + H[7];
+
+
+
+
+}
+
+
     uint32_t sigma_0(uint32_t x){
         //section 3.2
         //ROTR(x) = (x>>N) | (x <<(32-n))
         //shr_n(x) = (x >> n) 
+        return (rotr(7,x) ^ rotr(18,x) ^ shr(3,x));
     }
     uint32_t sigma_19(uint32_t x){
-
+        return (rotr(17,x) ^ rotr(19,x) ^ rotr(10,x));
     }
-     
-}
+    uint32_t rotr(uint32_t n, uint32_t x){
+        return (x >> n) | (x << (32 - n));
+    }
+    uint32_t shr(uint32_t n, uint32_t x){
+        return(x >> n);
+    }
