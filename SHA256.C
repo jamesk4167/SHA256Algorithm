efd0b1ac07af000eacf23bb7b9c2d31ad7289746
james kennedy <jameskennedy@jamess-MacBook-Air.local>
2019-03-14 19:01:55 +0000

    kept working on sha256

--- SHA256.C
+++ SHA256.C
@@ -14,10 +14,32 @@ void sha256();
 
 
 int main(int argc, char *argv[]){
+
+
+    sha256();
     return 0;
 }
 
 
 void sha256(){
+    uint32_t W[64];
+
+    uint32_t a,b,c,d,e,f,g,h;
+
     
+
+    uint32_t T1, T2;
+    uint32_t H[8] = {
+        0x6a09a667,
+        0xbb67ae85,
+        0x3c6ef372,
+        0xa54ff53a,
+        0x519e527f,
+        0x9b05688c,
+        0x1f83d9ab,
+        0x5be0cd19
+    };
+
+
+    uint32_t M[16]; 
 }
